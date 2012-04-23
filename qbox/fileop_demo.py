2ca88fe1cfb6e8df458c743d1f9971c1a59c3f2e
404 <awhy.xu@gmail.com>
2012-04-23 12:37:28 +0800

    fixed unit tests

--- qbox/fileop_demo.py
+++ qbox/fileop_demo.py
@@ -9,23 +9,23 @@ client = simpleoauth2.Client()
 client.ExchangeByPassword('test@qbox.net', 'test')
 
 tblName = 'tblName'
-key = '2.jpg'
+uniqkey = '2.jpg'
 
 rs = qboxrs.Service(client, tblName)
 
-resp = rs.Get(key, key)
-print '\n===> Get %s result:' % key
+resp = rs.Get(uniqkey, uniqkey)
+print '\n===> Get %s result:' % uniqkey
 print resp
 
 urlImageInfo = fileop.ImageInfoURL(resp['url'])
 
-print "\n===> ImageInfo of %s:\n" % key
+print "\n===> ImageInfo of %s:\n" % uniqkey
 print urllib.urlopen(urlImageInfo).read()
 print
 
 urlImagePreview = fileop.Image90x90URL(resp['url'])
 
-print "\n===> ImagePreview of %s:\n" % key
+print "\n===> ImagePreview of %s:\n" % uniqkey
 f = open("2.preview.jpg", "w")
 f.write(urllib.urlopen(urlImagePreview).read())
 f.close()
--- qbox/put_demo.py
+++ qbox/put_demo.py
@@ -4,24 +4,29 @@ import urllib
 import simpleoauth2
 import rs as qboxrs
 import rscli
+import time
 
 client = simpleoauth2.Client()
 client.ExchangeByPassword('test@qbox.net', 'test')
 
 tblName = 'tblName'
-key = 'put_demo.py'
+#uniqkey = 'errno-404'
+uniqkey = 'test-file-uniqkey-%f' % time.time()
 
 rs = qboxrs.Service(client, tblName)
 
 resp = rs.PutAuth()
-print '\n===> PutAuth %s result:' % key
+print '\n===> PutAuth %s result:' % uniqkey
 print resp
 
-resp = rscli.PutFile(resp['url'], tblName, key, '', __file__, 'CustomData', {'key': key})
-print '\n===> PutFile %s result:' % key
+resp = rscli.PutFile(resp['url'], tblName, uniqkey, '', __file__, 'CustomData', {'key': uniqkey})
+print '\n===> PutFile %s result:' % uniqkey
 print resp
 
-resp = rs.Stat(key)
-print '\n===> Stat %s result:' % key
+resp = rs.Stat(uniqkey)
+print '\n===> Stat %s result:' % uniqkey
 print resp
 
+resp = rs.Delete(uniqkey)
+print '\n===> Delete %s result:' % uniqkey
+print resp
