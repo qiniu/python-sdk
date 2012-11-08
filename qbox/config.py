e15483620c32d4313c3aa934b277a124f55bcd80
Feng Liyuan <darktemplar.f@gmail.com>
2012-11-08 15:16:51 +0800

    fix config.py

--- qbox/config.py
+++ qbox/config.py
@@ -16,36 +16,6 @@ RS_HOST  = 'http://rs.qbox.me:10100'
 EU_HOST  = 'http://eu.qbox.me'
 PUB_HOST = 'http://pu.qbox.me:10200'
 
-# production
-# IO_HOST  = 'http://iovip.qbox.me'
-# UP_HOST  = 'http://up.qbox.me'
-# RS_HOST  = 'http://rs.qbox.me:10100'
-# EU_HOST  = 'http://eu.qbox.me'
-# PUB_HOST = 'http://pu.qbox.me:10200'
-# TOKEN_ENDPOINT         = 'https://acc.qbox.me/oauth2/token'
-# ACCESS_KEY = 'hkmdGuWjK9IX9gjDVFfUN0TspZKnd3rvmuwXbku-'
-# SECRET_KEY = 'eg_qu6vkqkYQnEFmCBfVtyp0cwwQq6h1tKBq7wkt'
-
-# -d
-# IO_HOST  = 'http://m1.qbox.me:13004'
-# UP_HOST  = 'http://m1.qbox.me:13019'
-# RS_HOST  = 'http://m1.qbox.me:13003'
-# EU_HOST  = 'http://m1.qbox.me:13050'
-# PUB_HOST = 'http://m1.qbox.me:13012'
-# TOKEN_ENDPOINT         = 'http://m1.qbox.me:13001/oauth2/token'
-# ACCESS_KEY = 'FU6raNOrvI6MbuBvmm-byeKjX3hIBkbOHKFOVlMx'
-# SECRET_KEY = '4GdsUanm7ENL0BgStjJw6PjGTCeWYetVMOS83duy'
-
-# -l
-IO_HOST  = 'http://127.0.0.1:9200'
-UP_HOST  = 'http://127.0.0.1:11200'
-RS_HOST  = 'http://127.0.0.1:9400'
-EU_HOST  = 'http://127.0.0.1:15000'
-PUB_HOST = 'http://127.0.0.1:10200'
-TOKEN_ENDPOINT         = 'http://127.0.0.1:9100/oauth2/token'
-ACCESS_KEY = 'I8dA7P3FEkZNHUYWP9U3J-NEMuAiQ__yywrXsY49'
-SECRET_KEY = 'tfSgnY_zSm1dgtFa7wDXpiWvzKGLpopha0NX2Zfo'
-
 BLOCK_BITS = 22
 BLOCK_SIZE = 4194304
 PUT_CHUNK_SIZE = 262144
