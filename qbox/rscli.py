97bd1ef972144e4cd7fc6173e43591accab729a1
Feng Liyuan <darktemplar.f@gmail.com>
2012-12-20 17:20:34 +0800

    fix

--- qbox/rscli.py
+++ qbox/rscli.py
@@ -5,12 +5,12 @@ import urllib
 import urllib2
 from base64 import urlsafe_b64encode
 import config
-import zlib
+import utils
 
 
-def _crc32checksum(localFile):
-    with open(localFile, 'rb') as fh:
-        return zlib.crc32(fh.read())
+def __crc32checksum(localFile):
+    with file(localFile, 'rb') as fh:
+        return utils.crc32(fh.read())
 
 
 def PutFile(url, bucket, key, mimeType, localFile, customMeta='', callbackParams='', enable_crc32_check=False):
@@ -21,7 +21,7 @@ def PutFile(url, bucket, key, mimeType, localFile, customMeta='', callbackParams
     if customMeta != '':
         action += '/meta/' + urlsafe_b64encode(customMeta)
     if enable_crc32_check:
-        action += '/crc32/' + str(_crc32checksum(localFile))
+        action += '/crc32/' + str(__crc32checksum(localFile))
     params = {'file': file(localFile, 'rb'), 'action': action}
     if callbackParams != '':
         if isinstance(callbackParams, dict):
@@ -39,7 +39,7 @@ def UploadFile(bucket, key, mimeType, localFile, customMeta='', callbackParams='
     if customMeta != '':
         action += '/meta/' + urlsafe_b64encode(customMeta)
     if enable_crc32_check:
-        action += '/crc32/' + str(_crc32checksum(localFile))
+        action += '/crc32/' + str(__crc32checksum(localFile))
     params = {'action': action, 'file': file(localFile, 'rb')}
     if callbackParams != '':
         if isinstance(callbackParams, dict):
