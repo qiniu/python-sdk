ad5caf66b360ac1f49db7e16ec09e646ecfa54e2
Wanglin <dtynn@163.com>
2013-04-18 12:40:28 +0800

    新增UploadFileByBuffer\PutFileByBuffer

--- qbox/rscli.py
+++ qbox/rscli.py
@@ -6,14 +6,15 @@ import urllib2
 from base64 import urlsafe_b64encode
 import config
 
-def PutFile(url, bucket, key, mimeType, localFile, customMeta = '', callbackParams = ''):
+
+def PutFile(url, bucket, key, mimeType, localFile, customMeta='', callbackParams=''):
     if mimeType == '':
         mimeType = 'application/octet-stream'
     entryURI = bucket + ':' + key
     action = '/rs-put/' + urlsafe_b64encode(entryURI) + '/mimeType/' + urlsafe_b64encode(mimeType)
     if customMeta != '':
         action += '/meta/' + urlsafe_b64encode(customMeta)
-    params = {'file' : file(localFile, 'rb'), 'action': action}
+    params = {'file': file(localFile, 'rb'), 'action': action}
     if callbackParams != '':
         if isinstance(callbackParams, dict):
             callbackParams = urllib.urlencode(callbackParams)
@@ -21,14 +22,50 @@ def PutFile(url, bucket, key, mimeType, localFile, customMeta = '', callbackPara
     opener = urllib2.build_opener(MultipartPostHandler.MultipartPostHandler)
     return opener.open(url, params).read()
 
-def UploadFile(bucket, key, mimeType, localFile, customMeta = '', callbackParams = '', upToken = ''):
+
+def UploadFile(bucket, key, mimeType, localFile, customMeta='', callbackParams='', upToken=''):
     if mimeType == '':
         mimeType = 'application/octet-stream'
     entryURI = bucket + ':' + key
     action = '/rs-put/' + urlsafe_b64encode(entryURI) + '/mimeType/' + urlsafe_b64encode(mimeType)
     if customMeta != '':
         action += '/meta/' + urlsafe_b64encode(customMeta)
-    params = {'action': action, 'file' : file(localFile, 'rb')}
+    params = {'action': action, 'file': file(localFile, 'rb')}
+    if callbackParams != '':
+        if isinstance(callbackParams, dict):
+            callbackParams = urllib.urlencode(callbackParams)
+        params['params'] = callbackParams
+    if upToken != '':
+        params['auth'] = upToken
+    url = config.UP_HOST + "/upload"
+    opener = urllib2.build_opener(MultipartPostHandler.MultipartPostHandler)
+    return opener.open(url, params).read()
+
+
+def PutFile(url, bucket, key, mimeType, fileBuffer, customMeta='', callbackParams=''):
+    if mimeType == '':
+        mimeType = 'application/octet-stream'
+    entryURI = bucket + ':' + key
+    action = '/rs-put/' + urlsafe_b64encode(entryURI) + '/mimeType/' + urlsafe_b64encode(mimeType)
+    if customMeta != '':
+        action += '/meta/' + urlsafe_b64encode(customMeta)
+    params = {'file': fileBuffer, 'action': action}
+    if callbackParams != '':
+        if isinstance(callbackParams, dict):
+            callbackParams = urllib.urlencode(callbackParams)
+        params['params'] = callbackParams
+    opener = urllib2.build_opener(MultipartPostHandler.MultipartPostHandler)
+    return opener.open(url, params).read()
+
+
+def UploadFileByBuffer(bucket, key, mimeType, fileBuffer, customMeta='', callbackParams='', upToken=''):
+    if mimeType == '':
+        mimeType = 'application/octet-stream'
+    entryURI = bucket + ':' + key
+    action = '/rs-put/' + urlsafe_b64encode(entryURI) + '/mimeType/' + urlsafe_b64encode(mimeType)
+    if customMeta != '':
+        action += '/meta/' + urlsafe_b64encode(customMeta)
+    params = {'action': action, 'file': fileBuffer}
     if callbackParams != '':
         if isinstance(callbackParams, dict):
             callbackParams = urllib.urlencode(callbackParams)
