d4105f64802c9822bcd85382e638db528e509423
slene <vslene@gmail.com>
2014-05-16 17:26:10 +0800

    remove unused code

--- qiniu/rs/__init__.py
+++ qiniu/rs/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __all__ = [
-    "Client", "EntryPath", "EntryPathPair", "uri_stat", "uri_delete", "uri_move", "uri_copy",
     "PutPolicy", "GetPolicy", "make_base_url",
 ]
 
-from .rs import Client, EntryPath, EntryPathPair, uri_stat, uri_delete, uri_move, uri_copy
 from .rs_token import PutPolicy, GetPolicy, make_base_url
