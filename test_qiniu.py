7ce1ae47ed95ba742af13209abe14e034779fd28
forrest-mao <maolifu0304@126.com>
2014-11-24 23:17:49 +0800

    update test

--- test_qiniu.py
+++ test_qiniu.py
@@ -297,8 +297,10 @@ class ResumableUploaderTestCase(unittest.TestCase):
         qiniu.set_default(default_up_host=qiniu.config.UPAUTO_HOST)
 
 class DownloadTestCase(unittest.TestCase):
+    
+    q = Auth(access_key, secret_key)
+
     def test_private_url(self):
-        q = Auth(access_key, secret_key)
         bucket = 'test_private_bucket'
         key = 'test_private_key'
         key = key.encode('utf8')
