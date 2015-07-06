1e0b2ee879cd2cca5e086d0565e9781ac397f113
Haojian Wu <hokein.wu@gmail.com>
2015-07-06 19:11:22 +0800

    Generate record file name with base64.

--- qiniu/services/storage/upload_progress_recorder.py
+++ qiniu/services/storage/upload_progress_recorder.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import base64
 import json
 import os
 import tempfile
@@ -24,7 +25,9 @@ class UploadProgressRecorder(object):
         self.record_folder = record_folder
 
     def get_upload_record(self, key):
-        upload_record_file_path = os.path.join(self.record_folder, key)
+        record_file_name = base64.b64encode(key.encode('utf-8')).decode('utf-8')
+        upload_record_file_path = os.path.join(self.record_folder,
+                                               record_file_name)
         if not os.path.isfile(upload_record_file_path):
             return None
         with open(upload_record_file_path, 'r') as f:
@@ -32,13 +35,14 @@ class UploadProgressRecorder(object):
         return json_data
 
     def set_upload_record(self, key, data):
-        upload_record_file_path = os.path.join(self.record_folder, key)
-        folder = os.path.dirname(upload_record_file_path)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
+        record_file_name = base64.b64encode(key.encode('utf-8')).decode('utf-8')
+        upload_record_file_path = os.path.join(self.record_folder,
+                                               record_file_name)
         with open(upload_record_file_path, 'w') as f:
             json.dump(data, f)
 
     def delete_upload_record(self, key):
-        record_file_path = os.path.join(self.record_folder, key)
+        record_file_name = base64.b64encode(key.encode('utf-8')).decode('utf-8')
+        record_file_path = os.path.join(self.record_folder,
+                                        record_file_name)
         os.remove(record_file_path)
